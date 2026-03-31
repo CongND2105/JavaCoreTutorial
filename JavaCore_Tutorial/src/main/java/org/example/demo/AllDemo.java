267f9900a95199376b7c63a89637af871cb16c91
Tên account 1 <email_account1@gmail.com>
2026-03-31 23:11:39 +0700

    OOP Tutorial

--- JavaCore_Tutorial/src/main/java/org/example/demo/AllDemo.java
+++ JavaCore_Tutorial/src/main/java/org/example/demo/AllDemo.java
@@ -1,5 +1,8 @@
 package org.example.demo;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class AllDemo {
 
     // ví dụ về static method và non-static
@@ -13,6 +16,25 @@ public class AllDemo {
        AllDemo.hello();
        AllDemo demo = new AllDemo();
        demo.hello2();
+
+       // Array sort
+        int arr[]={9,2,5,1,10,12,634,6,23,21,65,1};
+        System.out.println(Arrays.toString(arr));
+        // for lồng nhau
+        // for i length -1 để tối ưu hoá , vì chỉ cần phần tử kế cuối, nếu ko -1 thì sẽ tốn thêm 1 lần lặp mà nó ko có phan tử phía sau để so sánh
+        for(int i = 0 ; i < arr.length-1 ;i++){
+            for(int j = i+1; j <arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp ;
+                }
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+        System.out.println("Mảng đã sắp xếp : " + Arrays.toString(arr));
+
+
     }
 
 
